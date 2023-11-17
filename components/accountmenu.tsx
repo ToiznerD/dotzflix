48cb6a6fe2cff919123ce4d1fead6d15f133e155
ToiznerD <toiznerd@gmail.com>
2023-11-17 13:42:38 +0200

    Add list of profiles to accountmenu

--- components/accountmenu.tsx
+++ components/accountmenu.tsx
@@ -1,5 +1,8 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+
+import useProfiles from "@/hooks/useProfiles";
+import { Profile } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from 'react';
 
 interface AccountMenuProps {
@@ -7,20 +10,30 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
-    const { data } = useCurrentUser();
-    if (!visible) {
+    const { data, isLoading } = useProfiles();
+    const router = useRouter();
+    const { profileId } = router.query
+    const singleCurrentProfile = Array.isArray(profileId) ? profileId[0] : profileId
+    if (!visible || isLoading) {
         return null;
     }
 
     return (
         <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
             <div className="flex flex-col gap-3">
-                <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
-                    <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
-                    <p className="text-white text-sm group-hover/item:underline">
-                        {data?.name}
-                    </p>
-                </div>
+                {data.map((profile: Profile) => (
+                    <div
+                        onClick={() => router.push(`/profile/${profile.id}`)}
+                        className="px-3 group/item flex flex-row gap-3 items-center w-full">
+                        <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
+                        <p className={profile.id === singleCurrentProfile ? "font-bold text-white text-md group-hover/item:underline"
+                            :
+                            "text-gray-500 text-sm group-hover/item:underline group-hover/item:text-white"}>
+                            {profile?.name}
+                        </p>
+                    </div>
+                ))}
+                
                 <hr className="bg-gray-600 border-0 h-px my-4" />
                 <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
                     Sign out of Netflix

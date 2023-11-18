f4011dbd139871ec760251e272327c7c1c2a5fef
ToiznerD <toiznerd@gmail.com>
2023-11-19 01:47:35 +0200

    Add TV Shows

---
+++ components/tvlist.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { isEmpty } from 'lodash'
+import TVCard from './tvcard';
+
+interface TVListProps {
+    data: Record<string, any>[];
+    title: string;
+}
+const TVList:React.FC<TVListProps> = ({ data, title }) => {
+    if (isEmpty(data)) {
+        return null;
+    }
+    return (
+        <div className="px-4 md:px-12 mt-4 space-y-8">
+            <div>
+                <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+                    {title}
+                </p>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 gap-x-4">
+                    {data.map((tv) => (
+                        <TVCard key={tv.id} data={tv} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TVList;
--- pages/profile/[profileId]/index.tsx
+++ pages/profile/[profileId]/index.tsx
@@ -7,11 +7,15 @@ import MovieList from '@/components/movielist';
 import useMovieList from '@/hooks/useMovieList';
 import useFavorites from '@/hooks/useFavorites';
 import InfoModal from '@/components/infomodal';
+import TvInfoModal from '@/components/tvinfomodal';
 import useInfoModal from '@/hooks/useInfoModal';
 import Search from '@/components/search';
 import useSearchModal from '@/hooks/useSearchModal';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import useTVList from '@/hooks/useTVList';
+import TVList from '@/components/tvlist';
+import useTvInfoModal from '@/hooks/useTvInfoModal';
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -31,11 +35,13 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: tvs = [] } = useTVList();
   const router = useRouter();
   const { profileId } = router.query;
   const singleProfileId = Array.isArray(profileId) ? profileId[0] : profileId;
   const { data: favorites = [] } = useFavorites(singleProfileId ?? '');
   const { isOpen, closeModal } = useInfoModal();
+  const { isOpen: isTvOpen, closeModal: closeTvModal } = useTvInfoModal();
   const { isOpen: isSearchOpen, closeModal: closeSearchModal } = useSearchModal();
 
     
@@ -45,12 +51,14 @@ export default function Home() {
   return (
       <>
       <InfoModal visible={isOpen} onClose={closeModal} />
+      <TvInfoModal visible={isTvOpen} onClose={closeTvModal} />
       <Search visible={isSearchOpen} onClose={closeSearchModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
-          <MovieList title="Trending Now" data={movies} />
-          <MovieList title="My List" data={favorites} />
+        <MovieList title="Trending Now" data={movies} />
+        <TVList title="Series" data={tvs} />
+        <MovieList title="My List" data={favorites} />
       </div>
       </>
   )
